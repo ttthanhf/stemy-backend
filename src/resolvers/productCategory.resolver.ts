3210a46eab5de489cf7a8675c4db36e3e6d75138
vphoa342 <vphoa34@gmail.com>
2024-09-24 13:42:10 +0700

    [STEM-58] Get Categories API

---
+++ src/resolvers/productCategory.resolver.ts
@@ -0,0 +1,18 @@
+import { GraphQLResolveInfo } from 'graphql';
+import { Info, Query, Resolver } from 'type-graphql';
+import { ProductCategory } from '~entities/product.entity';
+import { ProductCategoryService } from '~services/product.service';
+import { ResolverUtil } from '~utils/resolver.util';
+
+@Resolver()
+export class ProductCategoryResolver {
+	@Query(() => [ProductCategory])
+	async productCategories(@Info() info: GraphQLResolveInfo) {
+		const fields = ResolverUtil.getNodes(
+			info.fieldNodes[0].selectionSet?.selections
+		);
+		const categories =
+			await ProductCategoryService.getProductCategories(fields);
+		return categories;
+	}
+}
